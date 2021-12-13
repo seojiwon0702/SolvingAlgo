bf1bec65a83568b5c0a868b96cc367cf3174ad96
seojiwon0702 <seojiwon335@gmail.com>
2021-12-13 22:34:57 +0900

    update boj1916

--- Algorithm/src/week_17/Main_boj_1916_최소비용구하기.java
+++ Algorithm/src/week_17/Main_boj_1916_최소비용구하기.java
@@ -1,10 +1,20 @@
 package week_17;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main_boj_1916_최소비용구하기 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		
 	}
 
 }
