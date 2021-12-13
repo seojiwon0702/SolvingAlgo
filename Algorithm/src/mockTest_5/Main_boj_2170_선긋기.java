abf1f0fa9b75c5c5ef8552f36f20c629399c2df5
seojiwon0702 <seojiwon335@gmail.com>
2021-12-13 16:38:43 +0900

    init repository

--- Algorithm/src/mockTest_5/Main_boj_2170_선긋기.java
+++ Algorithm/src/mockTest_5/Main_boj_2170_선긋기.java
@@ -1,10 +1,71 @@
 package mockTest_5;
 
-public class Main_boj_2170_¼±±ß±â {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- 
-	} 
+public class Main_boj_2170_¼±±ß±â {
+	
+	static Queue<Line> queue = new PriorityQueue<Line>();
+	
+	private static int func() {
+		int start, end;
+		Line current = queue.poll();
+		start = current.start;
+		end = current.end;
+		int result = Math.abs(start-end);
+		while(!queue.isEmpty()) {
+			current = queue.poll();
+			if(current.start > end) {
+				result += Math.abs(current.start-current.end);
+				start = current.start;
+				end = current.end;
+			} else {
+				if(end<current.end) {
+					result+= Math.abs(current.end - end);
+					end = current.end;
+				}
+			}
+		}
+		
+		return result;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int N = Integer.parseInt(br.readLine());
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			queue.offer(new Line(start, end));
+		}
+		
+		System.out.println(func());
+		
+	}
+	
+	static class Line implements Comparable<Line> {
+		int start, end;
+		Line(int start, int end){
+			this.start = start;
+			this.end = end;
+		}
+		@Override
+		public int compareTo(Line o) {
+			if(this.start!=o.start) {
+				return Integer.compare(this.start, o.start);
+			} else {
+				return Integer.compare(this.end, o.end);
+			}
+		}
+		
+	}
 
 }
---
+++ Algorithm/src/week_17/Main_boj_1916_최소비용구하기.java
@@ -0,0 +1,10 @@
+package week_17;
+
+public class Main_boj_1916_최소비용구하기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
