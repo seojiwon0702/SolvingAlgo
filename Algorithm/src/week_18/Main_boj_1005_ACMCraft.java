ab62029aa28a91a2d67094ac6f425c56293f28cc
DESKTOP-C70T359\SEO <seojiwon335@gmail.com>
2021-12-24 02:52:05 +0900

    update boj_1005_ACM Craft.java

--- Algorithm/src/week_18/Main_boj_1005_ACMCraft.java
+++ Algorithm/src/week_18/Main_boj_1005_ACMCraft.java
@@ -3,10 +3,6 @@ package week_18;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_boj_1005_ACMCraft {
