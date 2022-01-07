b1b64b63e38db14b0cb6d71545fd5501a71eacd3
seojiwon0702 <seojiwon335@gmail.com>
2022-01-08 04:20:32 +0900

    update boj_1946_신입사원.java

--- Algorithm/src/v2_2/Main_boj_1946_신입사원.java
+++ Algorithm/src/v2_2/Main_boj_1946_신입사원.java
@@ -3,11 +3,25 @@ package v2_2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main_boj_1946_신입사원 {
 	
-	static int[][] scores = null;
+	private static int func(PriorityQueue<Applicant> applicants, int N) {
+		int answer = 0;
+		int maxInterviewRank = N+1;
+		
+		while(!applicants.isEmpty()) {
+			Applicant applicant = applicants.poll();
+			if(applicant.interview<maxInterviewRank) {
+				answer++;
+				maxInterviewRank = applicant.interview;
+			}
+		}
+		
+		return answer;
+	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,13 +31,29 @@ public class Main_boj_1946_신입사원 {
 		int T = Integer.parseInt(br.readLine());
 		for(int t=0; t<T; t++) {
 			int N = Integer.parseInt(br.readLine());
-			scores = new int[N][2];
+			PriorityQueue<Applicant> applicants = new PriorityQueue<Applicant>();
 			for(int i=0; i<N; i++) {
 				st = new StringTokenizer(br.readLine());
-				scores[i][0] = Integer.parseInt(st.nextToken());
-				scores[i][1] = Integer.parseInt(st.nextToken());
+				int document = Integer.parseInt(st.nextToken());
+				int interview = Integer.parseInt(st.nextToken());
+				applicants.offer(new Applicant(document, interview));
 			}
 			
+			sb.append(func(applicants, N)).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+	
+	static class Applicant implements Comparable<Applicant>{
+		int document, interview;
+		Applicant(int document, int interview){
+			this.document = document;
+			this.interview = interview;
+		}
+		
+		@Override
+		public int compareTo(Applicant o) {
+			return Integer.compare(this.document, o.document);
 		}
 	}
 
