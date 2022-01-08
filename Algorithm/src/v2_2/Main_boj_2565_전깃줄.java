d8c27a2d9802ee3b236c419371aff21f21b9c597
seojiwon0702 <seojiwon335@gmail.com>
2022-01-08 21:49:02 +0900

    add boj_2565_전깃줄.java

---
+++ Algorithm/src/v2_2/Main_boj_2565_전깃줄.java
@@ -0,0 +1,22 @@
+package v2_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_boj_2565_ภüฑ๊มู {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int N = Integer.parseInt(br.readLine());
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+		}
+	}
+
+}
