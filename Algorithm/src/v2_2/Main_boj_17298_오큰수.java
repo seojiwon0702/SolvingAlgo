1f14e83d0689ab32f18c204ebf38c92d8a309af9
seojiwon0702 <seojiwon335@gmail.com>
2022-01-08 21:49:30 +0900

    add boj_17298_오큰수.java

---
+++ Algorithm/src/v2_2/Main_boj_17298_오큰수.java
@@ -0,0 +1,13 @@
+package v2_2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_boj_17298_¿ÀÅ«¼ö {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
