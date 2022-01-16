db7aa0bc4318ddd7cf8002d02bb0556f5279878d
seojiwon0702 <seojiwon335@gmail.com>
2022-01-16 21:26:32 +0900

    add boj_1927_최소힙.java

---
+++ src/v2_3/Main_boj_1927_최소힙.java
@@ -0,0 +1,33 @@
+package v2_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main_boj_1927_최소힙 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++){
+            int n = Integer.parseInt(br.readLine());
+            if(n==0){
+                if(pq.size()==0)
+                    sb.append(0).append("\n");
+                else
+                    sb.append(pq.poll()).append("\n");
+            }
+            else
+                pq.offer(n);
+        }
+
+        System.out.println(sb.toString());
+    }
+}
+
