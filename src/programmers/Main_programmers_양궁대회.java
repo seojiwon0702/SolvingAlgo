ad146f7e184f1a94c3750b6b4e35c0bc82b1e120
DESKTOP-C70T359\SEO <seojiwon335@gmail.com>
2022-04-03 18:06:17 +0900

    update programmers 양궁대회

--- src/programmers/Main_programmers_양궁대회.java
+++ src/programmers/Main_programmers_양궁대회.java
@@ -1,47 +1,46 @@
 package programmers;
 
 public class Main_programmers_양궁대회 {
-    static int[] answer = new int[11];
     static int MAX = 0;
+    static int[] answer;
 
-    public static void dfs(int[] lion, int[] apeach, int scoreLion, int scoreApeach, int idx, int n){
-        if(idx==10){
-            if(scoreLion - scoreApeach >= MAX){
-                lion[idx] = n;
-                if(MAX < (scoreLion-scoreApeach)){
-                    MAX = scoreLion-scoreApeach;
-                    answer = lion.clone();
-                } else if(MAX == (scoreLion-scoreApeach)){
-                    for(int i=10; i>=0; i--){
-                        if(answer[i] < lion[i]) {
-                            answer = lion.clone();
-                            break;
-                        }
-                        else if(answer[i] == lion[i]) continue;
-                        else break;
-                    }
+    public static void dfs(int[] lion, int[] apeach, int n, int lionScore, int apeachScore, int idx) {
+        if (idx > 10) {
+            lion[10] = n;
+            if (lionScore - apeachScore > MAX) {
+                MAX = lionScore - apeachScore;
+                answer = lion.clone();
+            } else if (MAX != 0 && lionScore - apeachScore == MAX) {
+                for (int i = 10; i >= 0; i--) {
+                    if (answer[i] < lion[i]) {
+                        answer = lion.clone();
+                        break;
+                    } else if (answer[i] > lion[i])
+                        break;
                 }
             }
-        }
-        else {
-            if(n>apeach[idx]) {
+            return;
+        } else {
+            if (n > apeach[idx]) { // 이기는 경우
                 lion[idx] = apeach[idx] + 1;
-                dfs(lion, apeach, scoreLion + (10 - idx), scoreApeach, idx + 1, n - lion[idx]);
+                dfs(lion, apeach, n - (apeach[idx] + 1), lionScore + (10 - idx), apeachScore, idx + 1);
             }
-
             lion[idx] = 0;
-            dfs(lion, apeach, scoreLion, scoreApeach + (10 - idx), idx + 1, n);
+            if(apeach[idx]>0)
+                dfs(lion, apeach, n, lionScore, apeachScore + (10 - idx), idx + 1);
+            else dfs(lion, apeach, n, lionScore, apeachScore, idx+1);
         }
     }
 
     public static int[] solution(int n, int[] info) {
-        dfs(new int[11], info, 0, 0, 0, n);
-        return MAX>0? answer:new int[]{-1};
+        int[] lion = new int[11];
+        dfs(lion, info, n, 0, 0, 0);
+        return MAX > 0 ? answer : new int[]{-1};
     }
 
     public static void main(String[] args) {
-        int[] result = solution(5, new int[]{2,1,1,1,0,0,0,0,0,0,0});
-        for(int n : result)
+        int[] result = solution(5, new int[]{2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0});
+        for (int n : result)
             System.out.print(n + " ");
         System.out.println();
     }
