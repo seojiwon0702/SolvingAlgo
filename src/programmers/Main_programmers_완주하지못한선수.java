a6330144384f8bbc14590594aabfc261974332a1
seojiwon0702 <seojiwon335@gmail.com>
2022-09-27 16:06:22 +0900

    solved

--- src/programmers/Main_programmers_완주하지못한선수.java
+++ src/programmers/Main_programmers_완주하지못한선수.java
@@ -1,4 +1,25 @@
 package programmers;
 
+import java.util.Arrays;
+
 public class Main_programmers_완주하지못한선수 {
+    public static String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant); Arrays.sort(completion);
+        int i;
+        for ( i=0; i<completion.length; i++){
+            if (!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[i];
+    }
+
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+
+        String answer = solution(participant, completion);
+
+        System.out.println(answer);
+    }
 }
