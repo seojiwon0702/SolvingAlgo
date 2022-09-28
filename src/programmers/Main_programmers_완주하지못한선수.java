37cc9e8585bb4cdfde8be746497c40c16b53129a
seojiwon0702 <seojiwon335@gmail.com>
2022-09-28 18:16:43 +0900

    solved

--- src/programmers/Main_programmers_완주하지못한선수.java
+++ src/programmers/Main_programmers_완주하지못한선수.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class Main_programmers_완주하지못한선수 {
     public static String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant); Arrays.sort(completion);
+        Arrays.sort(participant);
+        Arrays.sort(completion);
         int i;
         for ( i=0; i<completion.length; i++){
             if (!participant[i].equals(completion[i])){
